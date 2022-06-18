3a8239b71122f4896d3af9d5dbc42380b4f545cd
Javier Fuentes <javierfuentes545@gmail.com>
2022-06-17 22:59:01 -0500

    [add] component tooltip

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -28,60 +28,167 @@ const Skill = () => {
     <div id="skills" className="container2">
       <h1 className="skill-txt">My Skills</h1>
       <ul className="container-skill">
-        <li className="skill-icon">
-          <SiHtml5 />
-        </li>
-        <li className="skill-icon">
-          <SiCss3 />
-        </li>
-        <li className="skill-icon">
-          <DiJavascript1 />
-        </li>
-        <li className="skill-icon">
-          <SiTypescript />
-        </li>
-        <li className="skill-icon">
-          <DiNodejs />
-        </li>
-        <li className="skill-icon">
-          <SiWebpack />
-        </li>
-        <li className="skill-icon">
-          <SiFirebase />
-        </li>
-        <li className="skill-icon">
-          <DiReact />
-        </li>
-        <li className="skill-icon">
-          <SiRedux />
-        </li>
-        <li className="skill-icon">
-          <DiSass />
-        </li>
-        <li className="skill-icon">
-          <SiMaterialui />
-        </li>
-        <li className="skill-icon">
-          <SiStyledcomponents />
-        </li>
-        <li className="skill-icon">
-          <SiBootstrap />
-        </li>
-        <li className="skill-icon">
-          <SiTailwindcss />
-        </li>
-        <li className="skill-icon">
-          <SiFigma />
-        </li>
-        <li className="skill-icon">
-          <SiAdobexd />
-        </li>
-        <li className="skill-icon">
-          <SiStrapi />
-        </li>
-        <li className="skill-icon">
-          <DiGit />
-        </li>
+        <div className="tooltip">
+          <li className="skill-icon">
+            <SiHtml5 />
+          </li>
+          <span className="tooltiptext">
+            <h3>HTML</h3>
+          </span>
+        </div>
+
+        <div className="tooltip">
+          <li className="skill-icon">
+            <SiCss3 />
+          </li>
+          <span className="tooltiptext">
+            <h3>CSS</h3>
+          </span>
+        </div>
+
+        <div className="tooltip">
+          <li className="skill-icon">
+            <DiJavascript1 />
+          </li>
+          <span className="tooltiptext">
+            <h3>JavaScript</h3>
+          </span>
+        </div>
+
+        <div className="tooltip">
+          <li className="skill-icon">
+            <SiTypescript />
+          </li>
+          <span className="tooltiptext">
+            <h3>TypeScript</h3>
+          </span>
+        </div>
+
+        <div className="tooltip">
+          <li className="skill-icon">
+            <DiNodejs />
+          </li>
+          <span className="tooltiptext">
+            <h3>NodeJS</h3>
+          </span>
+        </div>
+
+        <div className="tooltip">
+          <li className="skill-icon">
+            <SiWebpack />
+          </li>
+          <span className="tooltiptext">
+            <h3>Webpack</h3>
+          </span>
+        </div>
+
+        <div className="tooltip">
+          <li className="skill-icon">
+            <SiFirebase />
+          </li>
+          <span className="tooltiptext">
+            <h3>Firebase</h3>
+          </span>
+        </div>
+
+        <div className="tooltip">
+          <li className="skill-icon">
+            <DiReact />
+          </li>
+          <span className="tooltiptext">
+            <h3>ReactJS</h3>
+          </span>
+        </div>
+
+        <div className="tooltip">
+          <li className="skill-icon">
+            <SiRedux />
+          </li>
+          <span className="tooltiptext">
+            <h3>ReduxJS</h3>
+          </span>
+        </div>
+
+        <div className="tooltip">
+          <li className="skill-icon">
+            <DiSass />
+          </li>
+          <span className="tooltiptext">
+            <h3>Sass</h3>
+          </span>
+        </div>
+
+        <div className="tooltip">
+          <li className="skill-icon">
+            <SiMaterialui />
+          </li>
+          <span className="tooltiptext">
+            <h3>MaterialUI</h3>
+          </span>
+        </div>
+
+        <div className="tooltip">
+          <li className="skill-icon">
+            <SiStyledcomponents />
+          </li>
+          <span className="tooltiptext">
+            <h3>StyledComponent</h3>
+          </span>
+        </div>
+
+        <div className="tooltip">
+          <li className="skill-icon">
+            <SiBootstrap />
+          </li>
+          <span className="tooltiptext">
+            <h3>BootsTrapCSS</h3>
+          </span>
+        </div>
+
+        <div className="tooltip">
+          <li className="skill-icon">
+            <SiTailwindcss />
+          </li>
+          <span className="tooltiptext">
+            <h3>TailwindsCSS</h3>
+          </span>
+        </div>
+
+        <div className="tooltip">
+          <li className="skill-icon">
+            <SiFigma />
+          </li>
+          <span className="tooltiptext">
+            <h3>Figma</h3>
+          </span>
+        </div>
+
+        <div className="tooltip">
+          <li className="skill-icon">
+            <SiAdobexd />
+          </li>
+          <span className="tooltiptext">
+            <h3>AdobeXD</h3>
+          </span>
+        </div>
+
+        <div className="tooltip">
+          <li className="skill-icon">
+            <SiStrapi />
+          </li>
+          <span className="tooltiptext">
+            <h3>Strapi</h3>
+          </span>
+        </div>
+
+        <div className="tooltip">
+          <li className="skill-icon">
+            <DiGit />
+          </li>
+          <span className="tooltiptext">
+            <h3>Git</h3>
+          </span>
+        </div>
       </ul>
     </div>
   );
